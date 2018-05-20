c964b0c2d7635d6a3f5179bf38da449b9e545f1b
Tachyonscode <tachyonscode@gmail.com>
2018-05-19 21:13:12 -0300

    First Commit 2

---
+++ src/main/java/com/eventosapp/controllers/EventoController.java
@@ -0,0 +1,15 @@
+package com.eventosapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class EventoController {
+	
+	@RequestMapping("/cadastrarEvento")
+	public String form() {
+		return "evento/formEvento";
+	}
+
+}
